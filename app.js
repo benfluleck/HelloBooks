e4e117f8f48b15606e53df936de69ff457b38dd8
Benny <bo72@kent.ac.uk>
2017-07-25 15:03:34 +0100

    Dependencies installed; bootstrap, nodemon, jquery

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+// prepare server
+//app.use('/api', api); // redirect API calls
+//app.use('/', express.static(__dirname + '/www')); // redirect root
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
