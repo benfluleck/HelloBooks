cfb88adc63cc43410d624fe7ee4bfea4fca8a866
Benny Ogidan <bennyogidan@Andelas-MacBook-Pro.local>
2017-11-13 23:00:17 +0100

    fix: local Save added to project

---
+++ client/src/app/utils/localSave.js
@@ -0,0 +1,24 @@
+/*
+eslint-disable no-console
+*/
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (error) {
+    return;
+  }
+};
